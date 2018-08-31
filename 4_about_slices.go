8dfe0e83bd0de00cd794bc71d7bfa1444fbcb40b
Jon Povey <jon.povey@asos.com>
2018-08-31 12:16:42 +0100

    Complete about_slices

---
+++ 4_about_slices.go
@@ -0,0 +1,29 @@
+package go_koans
+
+import "fmt"
+
+func aboutSlices() {
+	fruits := []string{"apple", "orange", "mango"}
+
+	assert(fruits[0] == "apple") // slices seem like arrays
+	assert(len(fruits) == 3)     // in nearly all respects
+
+	tasty_fruits := fruits[1:3]         // we can even slice slices
+	assert(tasty_fruits[0] == "orange") // slices of slices also share the underlying data
+
+	pregnancy_slots := []string{"baby", "baby", "lemon"}
+	assert(cap(pregnancy_slots) == 3) // the capacity is initially the length
+
+	pregnancy_slots = append(pregnancy_slots, "baby!")
+	pregnancy_slots = append(pregnancy_slots, "baby2")
+	pregnancy_slots = append(pregnancy_slots, "baby3")
+	pregnancy_slots = append(pregnancy_slots, "baby4")
+	assert(len(pregnancy_slots) == 7) // slices can be extended with append(), much like realloc in C
+	fmt.Print(cap(pregnancy_slots))
+	assert(cap(pregnancy_slots) == 12) // but with better optimizations
+
+	pregnancy_slots = append(pregnancy_slots, "another baby!?", "yet another, oh dear!", "they must be Catholic")
+
+	assert(len(pregnancy_slots) == 10) // append() can take N arguments to append to the slice
+	assert(cap(pregnancy_slots) == 12) // the capacity optimizations have a guessable algorithm
+}
